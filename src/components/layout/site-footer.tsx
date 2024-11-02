51405a0fa38130278662cebc3cb0edc2a165c3e8
ShrideepP <shrideeppakhare2005@gmail.com>
2024-11-02 23:33:18 +0530

    minor ui change

--- src/components/layout/site-footer.tsx
+++ src/components/layout/site-footer.tsx
@@ -1,12 +1,12 @@
 import { Link } from "@tanstack/react-router";
 import LogoMark from "../common/logo-mark";
 import { buttonVariants } from "../ui/button";
-import { ExternalLink, Github } from "lucide-react";
+import { Github } from "lucide-react";
 import ToggleTheme from "../toggle-theme";
 
 const SiteFooter = () => {
   return (
-    <footer className="px-6 py-6 lg:px-36 lg:py-4 flex flex-col lg:flex-row lg:justify-between lg:items-center gap-y-6 border-t">
+    <footer className="px-6 py-6 lg:px-36 lg:py-4 flex justify-between items-center border-t">
       <Link to="/" className="inline-flex items-center gap-x-2">
         <LogoMark className="w-8 h-8 text-foreground" />
         <span className="text-base text-foreground font-bold">ByteBazaar</span>
